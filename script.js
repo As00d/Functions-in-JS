d2839e6e5db1d95bddcd3166bd1f4d169aad2301
Ankita Sood <ankisood@amazon.com>
2024-06-08 14:33:29 +0530

    Coding challenge 1

--- script.js
+++ script.js
@@ -164,11 +164,11 @@ console.log(euroWings);
 // One is using it with event listner
 vistara.planes = 300;
 console.log(vistara);
-vistara.buyPlane = function() {
+vistara.buyPlane = function () {
   console.log(this);
   this.planes++;
-  console.log(this.planes)
-}
+  console.log(this.planes);
+};
 
 const element = document.querySelector('.buy');
 element.addEventListener('click', vistara.buyPlane.bind(vistara));
@@ -179,24 +179,89 @@ console.log(vistara);
 // so what we have in here ?? We simply created a function taxCalculator now say for India tax this year is 20 so we want create a function keeping this value as default
 
 const taxCalculator = (tax, amount) => {
-return amount + amount*tax;
-}
-const taxCalculatorIndia = taxCalculator.bind(null,  20);
+  return amount + amount * tax;
+};
+const taxCalculatorIndia = taxCalculator.bind(null, 20);
 
 console.log(taxCalculatorIndia(100));
 
-// Now one might think we could have done it via default parameter but here in this case we r creating a new method itself what if you want to create another method for vietnam where tax = 0.13 so via default we had to pass this paramter but if create a new method out of generic method it is much better 
+// Now one might think we could have done it via default parameter but here in this case we r creating a new method itself what if you want to create another method for vietnam where tax = 0.13 so via default we had to pass this paramter but if create a new method out of generic method it is much better
 
 const taxCalculatorSpain = taxCalculator.bind(null, 0.13);
 console.log(taxCalculatorSpain(1339));
 
 // Reframe this method to create above scenario
 
-const taxCal = function(tax) {
-  return function(amount) {
-    return amount + amount*tax;
-  }
-}
+const taxCal = function (tax) {
+  return function (amount) {
+    return amount + amount * tax;
+  };
+};
 
 const val = taxCal(0.1);
 console.log(val(100));
+
+// Coding challenge 1
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  answers: new Array(4).fill(0),
+  registerNewAnswer: function () {
+    // user input
+    let value = Number(prompt(
+      `${this.question} \n${this.options.join('\n')} \n (Write option number)`
+    ));
+    // register answer - eg of short circuit also
+    typeof value === 'number' && value < this.answers.length && this.answers[value]++;
+    
+    this.displayResults('string');
+    this.displayResults('');
+  },
+  displayResults(type = 'array') {
+   
+    if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    } else if (type === 'array') {
+      console.log(...this.answers);
+    }
+  },
+};
+
+// Call this method whenever the user clicks the "Answer poll" button.
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// Bonus question resolution :
+
+ poll.displayResults.call({answers:[5,2,3]},'array');
