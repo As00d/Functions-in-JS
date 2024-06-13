9c70289dc8db4da81c4350ea93c620773d1ce7bf
Ankita Sood <ankisood@amazon.com>
2024-06-13 22:57:40 +0530

    Challenge closure

--- script.js
+++ script.js
@@ -360,9 +360,27 @@ const boardingTime = function (n, wait) {
   setTimeout(function () {
     console.log(`We are now boarding all ${n} passengers now!!`);
     console.log(`There are 3 groups each with ${perGroup} passengers`);
-  }, wait*1000);
+  }, wait * 1000);
   console.log(`Boarding will start in ${wait} seconds`);
 };
 
-boardingTime(300,3);
+boardingTime(300, 3);
 // closure also have priority over the scope chain
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
